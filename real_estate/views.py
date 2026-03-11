b3aedfa976e278e608148192001888301c3d6308
sugarfree-hash <sugarbusinessweb@gmail.com>
2026-03-11 21:11:59 +0900

    Ver.2.0

--- real_estate/views.py
+++ real_estate/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import HousePrice
 
-# Create your views here.
+def house_list(request):
+    houses = HousePrice.objects.all()
+
+    return render(request, 'real_estate/house_list.html', {'houses': houses})
+
